eb462289a16db7d03636608f944c931184741bba
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2021-01-29 23:28:10 +0530

    Addition Calculator
    A simple calculator. Only supports addition

---
+++ 2 basic-calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    cout << "Enter First number: ";
+    cin >> num1;
+    
+    cout << "enter second number: ";
+    cin >> num2;
+    
+    cout << num1 + num2;
+    return 0;
+}
