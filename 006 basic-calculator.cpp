d6fed07714cc02243b165df9c953a4bc52162bbe
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2021-03-27 17:42:14 +0530

    Added some comments

--- 006 basic-calculator.cpp
+++ 006 basic-calculator.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
 
+// A very basic calculator, which can be used
+// to enter two numbers and get the sum of them.
+
 int main()
 {
+    /* Using 'double' data type to manage
+       the decimal point numbers. You can also
+       use 'float' data type. */
+
     double num1, num2;
+
+    // Prompting the user to enter the 1st number.
     cout << "Enter First number: ";
     cin >> num1;
     
+    // Prompting to enter the 2nd number
     cout << "enter second number: ";
     cin >> num2;
     
+    // Printing out the sum.
     cout << num1 + num2;
     return 0;
 }
