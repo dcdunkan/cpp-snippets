24d25459263af2d528aa67b1eb0a9f721fca4a5a
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2021-03-27 17:48:05 +0530

    Added some comments to make clear how things works

--- 007 find-area-rectangle.cpp
+++ 007 find-area-rectangle.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 using namespace std;
 
+// Another example of how to use 'cin's and
+// 'cout's to input sides of a rectangle,
+// and print the area of the rectangle.
+
 int main()
 {
+  /* Initialising length, breadth, area varibles
+     as float data type for decimal support. You can
+     use 'double' also. */
+
   float length, breadth, area;
+
+  // Prompts to enter the length.
   cout << "Enter length of the rectangle: ";
   cin >> length;
+
+  // Prompts to enter the breadth(width)
   cout << "Enter breadth of the rectangle: ";
   cin >> breadth;
+
+  /* Demonstration of how to store some worked
+     datas to a variable. Storing the area as
+     multiple of length and breadth. */
   area = length * breadth;
+
+  // Printing out the area with some sentence.
   cout <<"Area of the rectangle = " << area;
 }
